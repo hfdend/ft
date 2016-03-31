9b8560b493294199c41bcec6d54af4ce1e6a472e
邓鸿风 <332401942@qq.com>
2016-03-31 18:24:52 +0800

    msg

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "github.com/hfdend/gohome/site"
+import (
+    "github.com/hfdend/gohome/site"
+    "github.com/hfdend/ft/ctl/index"
+)
 
 func main()  {
+    site.NewRouter().Add("/", "index.index")
+
+    site.RegisterCtl(&index.Index{})
+
     site.Run()
 }
